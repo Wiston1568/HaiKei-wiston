2c63bd2e6510d97e30ef52b8b4f75b29c807dd7c
wiston1568 <wistonanold2@gmail.com>
2026-03-15 16:46:29 +0000

    srver again

--- server.js
+++ server.js
@@ -30,30 +30,33 @@ const roomData = {};
 
 io.on("connection", (socket) => {
     let room;
-    setInterval(() => { if (roomData[room]) checkRoomMembers(roomData); }, 1000);
-    function createRoom(roomID) {
-        room = roomID;
-        socket.join(room);
-        roomData[room] = { users: {}, hostID: socket.id, currentTime: 0, roomID: roomID, isPublic: true, roomName: "Untitled Room", roomDescription: "No description provided.", playing: false, currentManifest: "", isVideoCurrentlyPlaying: false };
-        roomData[room].users[socket.id] = { ping: -1, id: socket.id, isHost: true };
+    // Safety check: only run interval if room exists
+    const interval = setInterval(() => { 
+        if (room && roomData[room]) checkRoomMembers(); 
+    }, 5000);
+
+    function checkRoomMembers() { 
+        if (roomData[room] && Object.keys(roomData[room].users).length < 1) {
+            delete roomData[room];
+        } 
     }
-    function joinRoom(roomID) {
-        room = roomID;
-        socket.join(room);
-        if (roomData[room]) {
-            roomData[room].users[socket.id] = { ping: -1, id: socket.id, isHost: false };
-            if (roomData[room].isVideoCurrentlyPlaying && roomData[room].currentManifest !== "") {
-                socket.emit("receiveUserVideoFromHost", {videoData: roomData[room].currentManifest, currentTime: roomData[room].currentTime});
-            }
-        }
-    }
+
     socket.on("init", (data) => {
-        if(io.sockets.adapter.rooms.get(data.roomID) && io.sockets.adapter.rooms.get(data.roomID).size > 0) joinRoom(data.roomID);
-        else createRoom(data.roomID);
+        room = data.roomID;
+        if(io.sockets.adapter.rooms.get(room) && io.sockets.adapter.rooms.get(room).size > 0) {
+            socket.join(room);
+            roomData[room].users[socket.id] = { ping: -1, id: socket.id, isHost: false };
+        } else {
+            socket.join(room);
+            roomData[room] = { users: {}, hostID: socket.id, currentTime: 0, roomID: room, isPublic: true, roomName: "Untitled Room", playing: false, currentManifest: "", isVideoCurrentlyPlaying: false };
+            roomData[room].users[socket.id] = { ping: -1, id: socket.id, isHost: true };
+        }
     });
-    socket.on('disconnect', () => { if (roomData[room]) delete roomData[room].users[socket.id]; });
-    function checkRoomMembers() { if (roomData[room] && Object.keys(roomData[room].users).length < 1) delete roomData[room]; }
-    socket.on("ping", (cb) => cb());
+
+    socket.on('disconnect', () => { 
+        if (room && roomData[room]) delete roomData[room].users[socket.id]; 
+        clearInterval(interval);
+    });
 });
 
 app.locals.pluralize = require('pluralize');
@@ -61,14 +64,11 @@ app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false,
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// Static files (CSS, JS, Images) from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// FIX: Improved Session Handling for Vercel
+// FIX: Session & Store
 let sessionStore;
 try {
-    // Vercel is read-only, we use /tmp for the temporary database file
     sessionStore = new SQLiteStore({ db: 'sessions.db', dir: '/tmp' });
 } catch (e) {
     sessionStore = null; 
@@ -85,7 +85,7 @@ app.use(session({
 app.use(flash());
 app.use(limit({max: 10, period: 5 * 1000, message: "Limit Exceeded!" }), passport.authenticate('session'));
 
-// FIX: Pointing EJS to the folder where your templates actually are
+// FIX: View Engine for HaiKei Structure
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public')); 
 app.engine('ejs', ejs.renderFile);
@@ -105,10 +105,9 @@ app.use('/genre/', require('./routers/genre/genre.js'));
 app.use("/status", require("./routers/status/status.js"));
 app.use('/watch', require("./routers/watch/watch.js"));
 
-// Using httpServer to allow Socket.io and Express to share the same port
-httpServer.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// FIX: Vercel specific listener
+if (process.env.NODE_ENV !== 'production') {
+    httpServer.listen(port, () => console.log(`Server running on port ${port}`));
+}
 
-function getRoomData() { return roomData; }
-module.exports = { getRoomData };
+module.exports = app;
